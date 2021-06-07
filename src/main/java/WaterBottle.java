4c39ccecf07cf5a0089466129a31e3463039b67f
Spencer <Spencer.Taber@yahoo.com>
2021-06-07 17:03:51 +0100

    fill bottle and empty bottle methods working

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -8,5 +8,13 @@ public class WaterBottle {
     public int removeWaterBy10(int num10) {
         return this.volume - num10;
     }
-}
 
+    public int emptyWaterBottle() {
+        return this.volume - this.volume;
+    }
+
+    public int fillWaterBottle() {
+        int difference = 100 - this.volume;
+        return this.volume + difference;
+    }
+}
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -20,4 +20,33 @@ public class WaterBottleTest {
           assertEquals(90, waterbottle.removeWaterBy10(num10));
       }
 
+      @Test
+    public void testEmptyWaterBottle(){
+
+          assertEquals(0, waterbottle.emptyWaterBottle());
+
+      }
+
+      @Test
+    public void testEmptyWaterBottleAfter10Removed(){
+          int num10 = 10;
+          assertEquals(90,waterbottle.removeWaterBy10(num10));
+          assertEquals(0, waterbottle.emptyWaterBottle());
+      }
+
+      @Test
+    public void testFullBottle(){
+          assertEquals(0, waterbottle.emptyWaterBottle());
+          assertEquals(100, waterbottle.fillWaterBottle());
+
+      }
+
+      @Test
+    public void testFullBottleAfter10Removed(){
+          int num10 = 10;
+          assertEquals(90,waterbottle.removeWaterBy10(num10));
+          assertEquals(100, waterbottle.fillWaterBottle());
+
+
+      }
 }
